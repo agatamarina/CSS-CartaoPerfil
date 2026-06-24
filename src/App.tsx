5bc5d660e55b7cd0e2b32358d78977b0d44e7ae3
agatamarina <agataarriaga@gmail.com>
2026-06-24 10:02:31 -0300

    primeiro

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// Src/App.tsx
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="card">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/4792/4792950.png"
+        alt="Foto de perfil"
+        className="profile-img"
+        width="150px"
+      />
+       <h2>Maria Silva</h2>
+      <p>Desenvolvedora Front-end apaixonada por React e CSS.</p>
+      <button className="btn">Seguir</button>
+    </div>
+  );
+}
+
+export default App
